7320f6d04c310d4fa74ee27fc22e82d381bf4e94
murilobispo <murilooliveirabispo90@gmail.com>
2025-12-30 19:06:25 -0300

    ♻️ Improving city search code

--- app.py
+++ app.py
@@ -6,37 +6,38 @@ st.set_page_config(page_title="Weather App",
                    layout="centered")
 
 def show_temperature(id):
-    st.write("lat: ",id["lat"])
-    st.write("lon: ",id["lon"])
+    st.write(id)
 
-def show_city_options(city):
+def search_city(city):
     headers={"User-Agent": "weather-app (github.com/murilobispo)"}
     url = f"https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&accept-language=en&q={city}"
-
     try:
         response = requests.get(url,headers=headers, timeout=5)
         response.raise_for_status()
         data = response.json()
-        pills_container = st.empty()
         if not data:
-            st.warning("CIty not found")
+            st.warning("City not found")
         else:
             df = pd.json_normalize(data)
             df = df[df["addresstype"].isin(["city", "municipality", "town"])].reset_index(drop=True)
             if df.empty:
                 st.warning("Enter only cities")
             elif len(df) == 1:
-                show_temperature(df.iloc[0])
+                show_temperature(df.iloc[0].to_dict())
             else:
-                options = (df["name"] + ", " + df["address.state"] + ", " + df["address.country"]).tolist()
-                with pills_container:
+                placeholder = st.empty()
+                addresses = (df["name"] + ", " + df["address.state"] + ", " + df["address.country"]).tolist()
+                options =  dict(zip(df["place_id"], addresses))
+                with placeholder:
                     selected = st.pills(label="Do you mean:", 
-                                        options=options, 
+                                        options=options.keys(),
+                                        format_func=lambda option: options[option],
                                         selection_mode="single",
+                                        key="city_pills"
                                         )
-                    if selected:
-                        pills_container.empty()
-                        show_city_options(selected)
+                if selected:
+                    placeholder.empty() 
+                    show_temperature(df.loc[df["place_id"] == selected].iloc[0].to_dict())
 
     except requests.exceptions.RequestException as e:
         st.error(f"Error {e}")
@@ -48,7 +49,7 @@ city_input = st.text_input(label="city-input",
                            placeholder="city"
                            )
 if city_input:
-    show_city_options(city_input)
+    search_city(city_input)
 
 #metric = st.metric(label="Temperature", 
                 #value="70 °F", )
